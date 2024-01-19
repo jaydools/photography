8b500691ae138b01f9150ec8c896d65973e1c84d
jaydools <134211868+jaydools@users.noreply.github.com>
2024-01-19 14:53:03 -0500

    layout added. will need to add links and change fonts later

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -2,7 +2,15 @@ import "./Navbar.scss";
 import React from "react";
 
 function Navbar() {
-    return <div>Navbar</div>;
+    return (
+        <nav>
+            <ul className="navbar">
+                <li className="navbar__link">Gallery</li>
+                <li className="navbar__link">About</li>
+                <li className="navbar__link">Contact</li>
+            </ul>
+        </nav>
+    );
 }
 
 export default Navbar;
