be46a77958e55cd087afc5a3cd377f7e0e031c44
jaydools <134211868+jaydools@users.noreply.github.com>
2024-01-18 20:42:03 -0500

    basic folder structure set up

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
+import Navbar from "./Components/Navbar/Navbar";
+import Hero from "./Components/Hero/Hero";
+
 function App() {
-    return <div className="App"></div>;
+    return (
+        <div className="App">
+            <Navbar />
+            <Hero />
+        </div>
+    );
 }
 
 export default App;
