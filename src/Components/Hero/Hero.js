cd723749a8be86c6b06ce249a5b3d97a51e8ad51
jaydools <134211868+jaydools@users.noreply.github.com>
2024-01-19 16:16:00 -0500

    begin working on hero img placeholders

--- src/Components/Hero/Hero.js
+++ src/Components/Hero/Hero.js
@@ -2,7 +2,22 @@ import React from "react";
 import "./Hero.scss";
 
 function Hero() {
-    return <div>Hero</div>;
+    return (
+        <section className="hero-container">
+            <div className="hero-left">
+                <h1 className="hero-left__header">Jordan Dooley</h1>
+                <h2 className="hero-left__subhead">Photography</h2>
+            </div>
+            <div className="hero-right">
+                <div className="hero-right__img1"></div>
+                <div className="hero-right__img2"></div>
+                <div className="hero-right__img3"></div>
+                <div className="hero-right__img4"></div>
+                <div className="hero-right__img5"></div>
+                <div className="hero-right__img6"></div>
+            </div>
+        </section>
+    );
 }
 
 export default Hero;
--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -3,12 +3,14 @@ import React from "react";
 
 function Navbar() {
     return (
-        <nav>
-            <ul className="navbar">
-                <li className="navbar__link">Gallery</li>
-                <li className="navbar__link">About</li>
-                <li className="navbar__link">Contact</li>
-            </ul>
+        <nav className="navbar-container">
+            <div className="navbar-container__wrapper">
+                <ul className="navbar">
+                    <li className="navbar__link">Gallery</li>
+                    <li className="navbar__link">About</li>
+                    <li className="navbar__link">Contact</li>
+                </ul>
+            </div>
         </nav>
     );
 }
