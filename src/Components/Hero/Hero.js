56a9935b1b90c2541de352d62ebfe984979270d2
jaydools <134211868+jaydools@users.noreply.github.com>
2024-01-20 19:11:27 -0500

    merging to develop

--- src/Components/Hero/Hero.js
+++ src/Components/Hero/Hero.js
@@ -9,12 +9,12 @@ function Hero() {
                 <h2 className="hero-left__subhead">Photography</h2>
             </div>
             <div className="hero-right">
-                <div className="hero-right__img1"></div>
-                <div className="hero-right__img2"></div>
-                <div className="hero-right__img3"></div>
-                <div className="hero-right__img4"></div>
-                <div className="hero-right__img5"></div>
-                <div className="hero-right__img6"></div>
+                <div className="hero-right__img1">1</div>
+                <div className="hero-right__img2">2</div>
+                <div className="hero-right__img3">3</div>
+                <div className="hero-right__img4">4</div>
+                <div className="hero-right__img5">5</div>
+                <div className="hero-right__img6">6</div>
             </div>
         </section>
     );
