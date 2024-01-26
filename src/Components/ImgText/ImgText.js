2f2e17f03d5168927cdca29a34295fe13f5d4a04
jaydools <134211868+jaydools@users.noreply.github.com>
2024-01-26 11:40:15 -0500

    moving to new branch

--- src/Components/ImgText/ImgText.js
+++ src/Components/ImgText/ImgText.js
@@ -4,8 +4,12 @@ import "./ImgText.scss";
 function ImgText() {
     return (
         <section className="img-text-container">
-            <div className="img-container">Image</div>
-            <div className="text-container">text</div>
+            <div className="img-container">Image side</div>
+            <div className="text-container">
+                Text side
+                <h2 className="text-container__header">Landscapes</h2>
+                <button className="text-container__button">View</button>
+            </div>
         </section>
     );
 }
