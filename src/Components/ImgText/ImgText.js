756bb687c1d61e7832756586dd2ffcdcf2fc2245
jaydools <134211868+jaydools@users.noreply.github.com>
2024-02-06 23:02:07 -0500

    got the logic done for one imgText container. Will pull h2 from album name, and display one image.
    need to figure out next cursor, and how to change h2 for different albums

--- src/Components/ImgText/ImgText.js
+++ src/Components/ImgText/ImgText.js
@@ -2,13 +2,14 @@ import React from "react";
 import "./ImgText.scss";
 import { Link } from "react-router-dom";
 
-function ImgText({ randoLandscapeImg }) {
-    // console.log(currentImages);
+function ImgText({ randoLandscapeImg, reverseOrder, categoryName }) {
+    const unoReverse = reverseOrder ? "img-text-container-reverse" : "img-text-container";
+
     return (
-        <section className="img-text-container">
+        <section className={unoReverse}>
             <img className="img-container" src={randoLandscapeImg} />
             <div className="text-container">
-                <h2 className="text-container__header">Landscapes</h2>
+                <h2 className="text-container__header">{categoryName}</h2>
                 <Link to="/landscapes">
                     <button className="text-container__button">View</button>
                 </Link>
--- src/Pages/Homepage/Homepage.js
+++ src/Pages/Homepage/Homepage.js
@@ -1,4 +1,4 @@
-import "./Homepage";
+import "./Homepage.scss";
 import axios from "axios";
 import { useEffect, useState } from "react";
 import Hero from "../../Components/Hero/Hero";
@@ -38,14 +38,30 @@ function Homepage() {
             return landscapeImages[randomIndex].secure_url;
         }
     };
-
+    const getCategoryName = (folder) => {
+        // Split the folder string by "/" and return the last part
+        const parts = folder.split("/");
+        // Remove the first half
+        const categoryName = parts[parts.length - 1];
+        // Return that, capitailize first
+        return categoryName.charAt(0).toUpperCase() + categoryName.slice(1);
+    };
     const randoLandscapeImg = getRandoLandscape();
+    const categoryName = getCategoryName("photography/landscapes");
 
     return (
         <main>
             <Hero />
-            <ImgText randoLandscapeImg={randoLandscapeImg} />
-            <ImgText />
+            <ImgText
+                randoLandscapeImg={randoLandscapeImg}
+                reverseOrder={false}
+                categoryName={categoryName}
+            />
+            <ImgText
+                randoLandscapeImg={randoLandscapeImg}
+                reverseOrder={true}
+                categoryName={categoryName}
+            />
         </main>
     );
 }
