885ee632d54cc39d4eb381667da3591ca5691f06
jaydools <134211868+jaydools@users.noreply.github.com>
2024-02-07 22:56:45 -0500

    logic completed for styling hero

--- src/Components/Hero/Hero.js
+++ src/Components/Hero/Hero.js
@@ -1,8 +1,36 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import "./Hero.scss";
 
 function Hero({ randoImg }) {
-    console.log(randoImg);
+    const imagesRef = useRef([]);
+
+    useEffect(() => {
+        const handleScroll = () => {
+            const scrollPosition = window.scrollY;
+            const scrollers = imagesRef.current;
+
+            if (scrollers && scrollers.length > 0) {
+                scrollers.forEach((image, index) => {
+                    const translateY = -150 * (scrollPosition / window.innerHeight);
+                    image.style.transform = `translateY(${translateY}px)`;
+                    // Apply animation effect dynamically based on scroll position
+                    if (scrollPosition > (index + 1) * 100) {
+                        image.classList.add("heroUpDown");
+                    } else {
+                        image.classList.remove("heroUpDown");
+                    }
+                });
+            }
+        };
+
+        window.addEventListener("scroll", handleScroll);
+
+        // Cleanup function to remove the event listener when the component unmounts
+        return () => {
+            window.removeEventListener("scroll", handleScroll);
+        };
+    }, []);
+
     return (
         <section className="hero-container">
             <div className="hero-left">
@@ -13,6 +41,8 @@ function Hero({ randoImg }) {
                 {randoImg.map((imageUrl, index) => (
                     <img
                         key={index}
+                        //impliment scroll movement with ref
+                        ref={(el) => (imagesRef.current[index] = el)}
                         className={`hero-right__img${index + 1}`}
                         src={imageUrl}
                         alt={`Random Image ${index + 1}`}
